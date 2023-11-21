08a530d8bd5e498deab76387a2c800b96139317e
xsw <wen.x.serena@hotmail.com>
2023-11-21 01:08:25 -0500

    added measure date to soil pit

--- lib/pages/soil_pit/soil_pit_summary_table.dart
+++ lib/pages/soil_pit/soil_pit_summary_table.dart
@@ -1,6 +1,9 @@
+import 'package:drift/drift.dart' as d;
 import 'package:survey_app/barrels/page_imports_barrel.dart';
+import 'package:survey_app/providers/soil_pit_providers.dart';
 
 import '../../providers/survey_info_providers.dart';
+import '../../widgets/date_select.dart';
 
 class SoilPitSummaryPage extends ConsumerStatefulWidget {
   static const String routeName = "soilPitSummary";
@@ -29,10 +32,20 @@ class SoilPitSummaryPageState extends ConsumerState<SoilPitSummaryPage> {
     super.initState();
   }
 
+  Future<void> updateSpSummary(SoilPitSummaryCompanion entry) async {
+    final db = ref.read(databaseProvider);
+    (db.update(db.soilPitSummary)..where((t) => t.id.equals(spId)))
+        .write(entry);
+    ref.refresh(soilSiteInfoDataProvider(surveyId));
+  }
+
   @override
   Widget build(BuildContext context) {
     final db = ref.read(databaseProvider);
-    debugPrint("Going to ${GoRouterState.of(context).uri.toString()}");
+
+    AsyncValue<SoilPitSummaryData> spSummary =
+        ref.watch(soilSiteInfoDataProvider(surveyId));
+
     return Scaffold(
       appBar: OurAppBar(
         title,
@@ -41,6 +54,24 @@ class SoilPitSummaryPageState extends ConsumerState<SoilPitSummaryPage> {
           context.pop();
         },
       ),
+      endDrawer: DrawerMenu(onLocaleChange: () => setState(() {})),
+      body: Center(
+          child: spSummary.when(
+        error: (err, stack) => Text("Error: $err"),
+        loading: () => const Center(child: CircularProgressIndicator()),
+        data: (spSummary) => Column(
+          children: [
+            CalendarSelect(
+              date: spSummary.measDate,
+              label: "Enter Measurement Date",
+              readOnly: spSummary.complete,
+              readOnlyPopup: completeWarningPopup,
+              setStateFn: (DateTime date) async => updateSpSummary(
+                  SoilPitSummaryCompanion(measDate: d.Value(date))),
+            ),
+          ],
+        ),
+      )),
     );
   }
 }
