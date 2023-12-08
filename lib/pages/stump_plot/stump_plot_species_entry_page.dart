c10f48934b08647568842c706d01df986ad63c91
xsw <wen.x.serena@hotmail.com>
2023-12-08 02:23:46 -0500

    added new version code

--- lib/pages/stump_plot/stump_plot_species_entry_page.dart
+++ lib/pages/stump_plot/stump_plot_species_entry_page.dart
@@ -1,4 +1,10 @@
 import 'package:survey_app/barrels/page_imports_barrel.dart';
+import 'package:survey_app/widgets/builders/decay_class_select_builder.dart';
+
+import '../../widgets/buttons/delete_button.dart';
+import '../../widgets/data_input/data_input.dart';
+import '../../widgets/dropdowns/drop_down_default.dart';
+import '../../widgets/hide_info_checkbox.dart';
 
 class StumpPlotSpeciesEntryPage extends ConsumerStatefulWidget {
   static const String routeName = "stumpPlotSpeciesEntry";
@@ -16,6 +22,114 @@ class StumpPlotSpeciesEntryPageState
   Widget build(BuildContext context) {
     final db = ref.read(databaseProvider);
     debugPrint("Going to ${GoRouterState.of(context).uri.toString()}");
-    return const Placeholder();
+    return Scaffold(
+      appBar: const OurAppBar(
+        "Stump Plot Species Entry",
+      ),
+      endDrawer: DrawerMenu(onLocaleChange: () {}),
+      body: Padding(
+        padding: const EdgeInsets.all(kPaddingH),
+        child: Center(
+          child: ListView(children: [
+            DropDownDefault(
+                title: "Original plot area",
+                onChangedFn: (s) {},
+                itemsList: [],
+                selectedItem: "Please select plot area"),
+            DropDownDefault(
+                title: "Genus",
+                onChangedFn: (s) {},
+                itemsList: [],
+                selectedItem: "Please select genus"),
+            DropDownDefault(
+                title: "Species",
+                onChangedFn: (s) {},
+                itemsList: [],
+                selectedItem: "Please select species"),
+            DropDownDefault(
+                title: "Variety",
+                onChangedFn: (s) {},
+                itemsList: [],
+                selectedItem: "Please select variety"),
+            HideInfoCheckbox(
+              title: "Stump DIB",
+              checkTitle: "Missing",
+              checkValue: false,
+              child: DataInput(
+                  boxLabel: "Top inside bark diameter of stump in cm.",
+                  generalPadding: const EdgeInsets.only(top: 0),
+                  onSubmit: (s) {},
+                  onValidate: (s) {}),
+            ),
+            HideInfoCheckbox(
+              title: "Stump diameter",
+              checkTitle: "Missing",
+              checkValue: false,
+              child: DataInput(
+                  boxLabel: "Top diameter of stump including bark, if present. "
+                      "If no bark present then STUMP_DIAMETER = STUMP_DIB. "
+                      "Reported in cm.",
+                  generalPadding: const EdgeInsets.only(top: 0),
+                  onSubmit: (s) {},
+                  onValidate: (s) {}),
+            ),
+            HideInfoCheckbox(
+              title: "Stump length",
+              checkTitle: "Missing",
+              checkValue: false,
+              child: DataInput(
+                  boxLabel: "Length, measured to the nearest 0.01 m.",
+                  generalPadding: const EdgeInsets.only(top: 0),
+                  onSubmit: (s) {},
+                  onValidate: (s) {}),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: kPaddingV * 2),
+              child: DecayClassSelectBuilder(
+                  title: "Decay Class", onChangedFn: (s) {}, selectedItem: ""),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: kPaddingV * 2),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                      onPressed: () => null,
+                      child: const Text("Save and return")),
+                  ElevatedButton(
+                      onPressed: () => null,
+                      child: const Text("Save and Add New Feature")),
+                ],
+              ),
+            ),
+            true
+                ? DeleteButton(
+                    delete: () => Popups.show(
+                      context,
+                      PopupContinue("Warning: Deleting Stump Plot Species",
+                          contentText: "You are about to delete this feature. "
+                              "Are you sure you want to continue?",
+                          rightBtnOnPressed: () {
+                        //close popup
+                        context.pop();
+                        // context.pushNamed(DeletePage.routeName, extra: {
+                        //   DeletePage.keyObjectName:
+                        //   "Soil Pit Feature: ${feature.toString()}",
+                        //   DeletePage.keyDeleteFn: () {
+                        //     (db.delete(db.soilPitFeature)
+                        //       ..where(
+                        //               (tbl) => tbl.id.equals(feature.id.value)))
+                        //         .go()
+                        //         .then((value) => goToFeaturePage());
+                        //   },
+                        // });
+                      }),
+                    ),
+                  )
+                : Container()
+          ]),
+        ),
+      ),
+    );
   }
 }
--- lib/pages/stump_plot/stump_plot_summary_page.dart
+++ lib/pages/stump_plot/stump_plot_summary_page.dart
@@ -1,4 +1,45 @@
 import 'package:survey_app/barrels/page_imports_barrel.dart';
+import 'package:survey_app/pages/stump_plot/stump_plot_species_entry_page.dart';
+import 'package:syncfusion_flutter_datagrid/datagrid.dart';
+
+import '../../widgets/data_input/data_input.dart';
+import '../../widgets/date_select.dart';
+import '../../widgets/dropdowns/drop_down_default.dart';
+import '../../widgets/tables/table_creation_builder.dart';
+import '../../widgets/tables/table_data_grid_source_builder.dart';
+import '../../wrappers/column_header_object.dart';
+
+class ColNames {
+  ColNames();
+  ColumnHeaders id = ColumnHeaders(ColumnHeaders.headerNameId, visible: false);
+  ColumnHeaders stumpNumber = ColumnHeaders("Stump Number");
+  ColumnHeaders originalPlotArea = ColumnHeaders("Original Plot Area");
+  ColumnHeaders genus = ColumnHeaders("Genus");
+  ColumnHeaders species = ColumnHeaders("Species");
+  ColumnHeaders variety = ColumnHeaders("Variety");
+  ColumnHeaders topDiameterInsideBark =
+      ColumnHeaders("Top Diameter Inside Bark");
+  ColumnHeaders topDiameterIncludingBark =
+      ColumnHeaders("Top Diameter Including Bark");
+  ColumnHeaders stumpLength = ColumnHeaders("Stump Length");
+  ColumnHeaders decayClass = ColumnHeaders("Decay Class");
+  ColumnHeaders edit = ColumnHeaders(ColumnHeaders.headerNameEdit, sort: false);
+
+  static String empty = "-";
+
+  List<ColumnHeaders> getColHeadersList() => [
+        stumpNumber,
+        originalPlotArea,
+        genus,
+        species,
+        variety,
+        topDiameterInsideBark,
+        topDiameterIncludingBark,
+        stumpLength,
+        decayClass,
+        edit,
+      ];
+}
 
 class StumpPlotSummaryPage extends ConsumerStatefulWidget {
   static const String routeName = "stumpPlotSummary";
@@ -10,10 +51,119 @@ class StumpPlotSummaryPage extends ConsumerStatefulWidget {
 }
 
 class StumpPlotSummaryPageState extends ConsumerState<StumpPlotSummaryPage> {
+  final ColNames columnData = ColNames();
+
+  List<DataGridRow> generateDataGridRows(List<String> plots) {
+    return [
+      DataGridRow(cells: [
+        DataGridCell<String>(columnName: columnData.id.name, value: "tmp"),
+        DataGridCell<String>(
+            columnName: columnData.stumpNumber.name, value: "tmp"),
+        DataGridCell<String>(
+            columnName: columnData.originalPlotArea.name, value: "tmp"),
+        DataGridCell<String>(columnName: columnData.genus.name, value: "tmp"),
+        DataGridCell<String>(columnName: columnData.species.name, value: "tmp"),
+        DataGridCell<String>(columnName: columnData.variety.name, value: "tmp"),
+        DataGridCell<String>(
+            columnName: columnData.topDiameterInsideBark.name, value: "tmp"),
+        DataGridCell<String>(
+            columnName: columnData.topDiameterIncludingBark.name, value: "tmp"),
+        DataGridCell<String>(
+            columnName: columnData.stumpLength.name, value: "tmp"),
+        DataGridCell<String>(
+            columnName: columnData.decayClass.name, value: "tmp"),
+        DataGridCell<String>(columnName: columnData.edit.name, value: "edit"),
+      ])
+    ];
+  }
+
+  DataGridSourceBuilder getSourceBuilder(List<String> stations) {
+    DataGridSourceBuilder source =
+        DataGridSourceBuilder(dataGridRows: generateDataGridRows(stations));
+    // source.sortedColumns.add(SortColumnDetails(
+    //     name: columnData.stationNum.toString(),
+    //     sortDirection: DataGridSortDirection.ascending));
+    // source.sort();
+
+    return source;
+  }
+
   @override
   Widget build(BuildContext context) {
     final db = ref.read(databaseProvider);
     debugPrint("Going to ${GoRouterState.of(context).uri.toString()}");
-    return const Placeholder();
+    return Scaffold(
+      appBar: OurAppBar(
+        "Stump Plot",
+        backFn: () {
+          //ref.refresh(updateSurveyCardProvider(surveyId));
+          context.pop();
+        },
+      ),
+      endDrawer: DrawerMenu(onLocaleChange: () => setState(() {})),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+              vertical: kPaddingV * 2, horizontal: kPaddingH / 2),
+          child: ListView(
+            children: [
+              CalendarSelect(
+                date: DateTime.now(),
+                label: "Enter Measurement Date",
+                readOnly: false,
+                setStateFn: (DateTime date) async => (),
+              ),
+              DropDownDefault(
+                  title: "Plot type",
+                  onChangedFn: (s) {},
+                  itemsList: [],
+                  selectedItem: "Please select plot type"),
+              DataInput(
+                  title: "Nominal Plot Size",
+                  onSubmit: (s) {},
+                  onValidate: (s) {}),
+              DataInput(
+                  title: "Measured Plot Size",
+                  onSubmit: (s) {},
+                  onValidate: (s) {}),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: kPaddingV * 2, horizontal: kPaddingH / 2),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "Species",
+                      style: TextStyle(fontSize: kTextTitleSize),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: kPaddingH),
+                      child: ElevatedButton(
+                          onPressed: () async => context.pushNamed(
+                              StumpPlotSpeciesEntryPage.routeName,
+                              pathParameters: widget.state.pathParameters),
+                          style: ButtonStyle(
+                              backgroundColor: false
+                                  ? MaterialStateProperty.all<Color>(
+                                      Colors.grey)
+                                  : null),
+                          child: const Text("Add species")),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: TableCreationBuilder(
+                  source: getSourceBuilder(["hi"]),
+                  colNames: columnData.getColHeadersList(),
+                  onCellTap: (DataGridCellTapDetails details) {},
+                  columnWidthMode: ColumnWidthMode.fitByColumnName,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
