e8b2b10b3ddfc1d8ccb9961cf8626b3db60b16eb
xsw <wen.x.serena@hotmail.com>
2023-12-27 00:33:01 -0500

    added scaffold page to ltp

--- lib/pages/large_tree_plot/large_tree_plot_summary.dart
+++ lib/pages/large_tree_plot/large_tree_plot_summary.dart
@@ -1,5 +1,7 @@
 import 'package:survey_app/barrels/page_imports_barrel.dart';
 
+import '../../widgets/date_select.dart';
+
 class LargeTreePlotSummaryPage extends ConsumerStatefulWidget {
   static const String routeName = "largeTreePlotSummary";
   final GoRouterState state;
@@ -16,6 +18,76 @@ class LargeTreePlotSummaryPageState
   Widget build(BuildContext context) {
     final db = ref.read(databaseProvider);
     debugPrint("Going to ${GoRouterState.of(context).uri.toString()}");
-    return const Placeholder();
+    return Scaffold(
+      appBar: OurAppBar(
+        "Large Tree Plot",
+        backFn: () {
+          //ref.refresh(updateSurveyCardProvider(surveyId));
+          context.pop();
+        },
+      ),
+      endDrawer: DrawerMenu(onLocaleChange: () => setState(() {})),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+              vertical: kPaddingV * 2, horizontal: kPaddingH / 2),
+          child: ListView(
+            children: [
+              CalendarSelect(
+                date: DateTime.now(),
+                label: "Enter Measurement Date",
+                readOnly: false,
+                setStateFn: (DateTime date) async => (),
+              ),
+              // DropDownDefault(
+              //     title: "Plot type",
+              //     onChangedFn: (s) {},
+              //     itemsList: [],
+              //     selectedItem: "Please select plot type"),
+              // DataInput(
+              //     title: "Nominal Plot Size",
+              //     onSubmit: (s) {},
+              //     onValidate: (s) {}),
+              // DataInput(
+              //     title: "Measured Plot Size",
+              //     onSubmit: (s) {},
+              //     onValidate: (s) {}),
+              // Padding(
+              //   padding: const EdgeInsets.symmetric(
+              //       vertical: kPaddingV * 2, horizontal: kPaddingH / 2),
+              //   child: Row(
+              //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //     children: [
+              //       const Text(
+              //         "Species",
+              //         style: TextStyle(fontSize: kTextTitleSize),
+              //       ),
+              //       Padding(
+              //         padding: const EdgeInsets.only(left: kPaddingH),
+              //         child: ElevatedButton(
+              //             onPressed: () async => context.pushNamed(
+              //                 SmallTreeSpeciesEntryPage.routeName,
+              //                 pathParameters: widget.state.pathParameters),
+              //             style: ButtonStyle(
+              //                 backgroundColor: false
+              //                     ? MaterialStateProperty.all<Color>(
+              //                     Colors.grey)
+              //                     : null),
+              //             child: const Text("Add species")),
+              //       ),
+              //     ],
+              //   ),
+              // ),
+              // Expanded(
+              //   child: TableCreationBuilder(
+              //       source: getSourceBuilder(["hi"]),
+              //       colNames: columnData.getColHeadersList(),
+              //       onCellTap: (DataGridCellTapDetails details) {}),
+              // )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
