9bebc7bfb6e1816f98a256b9b27768ee09c2fbf7
xsw <wen.x.serena@hotmail.com>
2024-01-17 04:07:07 -0500

    only treatment left

--- lib/pages/ground_plot/ground_plot_origin_entry_page.dart
+++ lib/pages/ground_plot/ground_plot_origin_entry_page.dart
@@ -1,4 +1,6 @@
 import 'package:survey_app/barrels/page_imports_barrel.dart';
+import 'package:survey_app/widgets/data_input/data_input.dart';
+import 'package:survey_app/widgets/dropdowns/drop_down_default.dart';
 
 class GroundPlotOriginEntryPage extends ConsumerStatefulWidget {
   static const String routeName = "groundPlotOriginEntry";
@@ -19,13 +21,29 @@ class GroundPlotOriginEntryPageState
     return Scaffold(
       appBar: const OurAppBar("Tree Cover Origin Entry"),
       endDrawer: DrawerMenu(onLocaleChange: () => setState(() {})),
+      bottomNavigationBar:
+          ElevatedButton(child: Text("Save"), onPressed: () => null),
       body: Center(
           child: Padding(
         padding: const EdgeInsets.symmetric(
             vertical: kPaddingV * 2, horizontal: kPaddingH / 2),
-        child: ElevatedButton(
-          child: const Text("tmp"),
-          onPressed: () {},
+        child: ListView(
+          children: [
+            DropDownDefault(
+                title: "Vegetation cover origin",
+                onChangedFn: (s) {},
+                itemsList: ["SUCC", "HARV"],
+                selectedItem: ""),
+            DropDownDefault(
+                title: "Type of regeneration",
+                onChangedFn: (s) {},
+                itemsList: ["NAT", "SUP"],
+                selectedItem: ""),
+            DataInput(
+                title: "Year of regeneration",
+                onSubmit: (s) {},
+                onValidate: (s) {}),
+          ],
         ),
       )),
     );
